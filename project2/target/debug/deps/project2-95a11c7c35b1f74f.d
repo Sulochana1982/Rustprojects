c0032e29fc5a6247faf51b095b47fbb37cf08e50
sulochana1982 <sulochanajayaprakash@gmail.com>
2023-06-06 09:45:44 +0530

    basic to test program

---
+++ project2/target/debug/deps/project2-95a11c7c35b1f74f.d
@@ -0,0 +1,5 @@
+/Users/sulochanajayaprakash/Desktop/rustproject1/project2/target/debug/deps/project2-95a11c7c35b1f74f: src/main.rs
+
+/Users/sulochanajayaprakash/Desktop/rustproject1/project2/target/debug/deps/project2-95a11c7c35b1f74f.d: src/main.rs
+
+src/main.rs:
---
+++ project2/target/debug/project2.d
@@ -0,0 +1,1 @@
+/Users/sulochanajayaprakash/Desktop/rustproject1/project2/target/debug/project2: /Users/sulochanajayaprakash/Desktop/rustproject1/project2/src/main.rs
