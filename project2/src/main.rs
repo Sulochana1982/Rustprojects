7f6647166ad844484f4de4b9c2839bd00c460491
sulochana1982 <sulochanajayaprakash@gmail.com>
2023-06-08 09:10:55 +0530

    commit

--- project2/src/main.rs
+++ project2/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    
+    
+        println!("{}", 1);
+        println!("{}{}", 2 , 2);
+        println!("{}{}{}", 3 , 3 ,3);
+        println!("{}{}{}{}", 4 ,4 , 4 , 4);
+        println!("{}{}{}{}{}", 5 , 5, 5, 5, 5)
+     
 }
